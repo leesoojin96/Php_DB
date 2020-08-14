54b7353a470f8f2dcc2f9fcb7c0358929949e18f
leesoojin96 <69374300+leesoojin96@users.noreply.github.com>
2020-08-14 18:11:25 +0900

    Add files via upload

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+$a = array("About JavaScript","javasciprt is~");
+echo $a[0];
+
+echo "<br />";
+echo "<h1>연관배열:associative array</h1>";
+//  => : 타이틀이 About JavaScript 이라는 정보를 갖고 있다는 의미
+$b = array("title"=>"About JavaScript","description"=>"JavaScript is~");
+echo $b["title"];
+?>
